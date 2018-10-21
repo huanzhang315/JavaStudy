67e88da33c5534b8076f3189d857cab8aabfbe44
admin <admin@admins-MacBook-Pro-2.local>
2018-10-21 13:21:26 -0700

    my first java

---
+++ test.java
@@ -0,0 +1,9 @@
+package com.company.huan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello world!");
+    }
+}
